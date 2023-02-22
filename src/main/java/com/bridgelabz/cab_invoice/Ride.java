d4a4d4d9d5d2cfc7199c6fa6ce09e33701cb6fdc
Mrk0p <sachinwele23@gmail.com>
2023-02-22 19:43:42 +0530

    Added:cab invoice generator for multiple rides

---
+++ src/main/java/com/bridgelabz/cab_invoice/Ride.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.cab_invoice;
+
+public class Ride {
+
+    double distance ;
+
+    double time ;
+
+    public Ride(double distance, double time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}

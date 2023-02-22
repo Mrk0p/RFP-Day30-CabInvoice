71d3a553cb621a67e80d2a22280b832f109a1dee
Mrk0p <sachinwele23@gmail.com>
2023-02-22 19:40:25 +0530

    Added:cab invoice generator for total fare

---
+++ src/test/java/com/bridgelabz/cab_invoice/CabInvoiceTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.cab_invoice;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class CabInvoiceTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare(){
+        CabInvoice cabInvoice = new CabInvoice();
+        double totalFare = cabInvoice.calculateTotalFare(10.0,4.0);
+        Assertions.assertEquals(104.0,totalFare);
+    }
+}
